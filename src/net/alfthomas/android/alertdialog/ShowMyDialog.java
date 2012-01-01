0869d512860fe367f8544a613842dfa717e053d3
alfthomas <alf.thomas.nilsen@gmail.com>
2012-01-01 18:21:39 +0100

    Implemented a simple alert dialog

--- src/net/alfthomas/android/alertdialog/ShowMyDialog.java
+++ src/net/alfthomas/android/alertdialog/ShowMyDialog.java
@@ -1,7 +1,13 @@
 package net.alfthomas.android.alertdialog;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ShowMyDialog extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +15,36 @@ public class ShowMyDialog extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void openMyDialog(View view) {
+        showDialog(10);
+    }
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch(id) {
+            case 10:
+                Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("This will end the activity");
+                builder.setCancelable(true);
+                builder.setPositiveButton("I agree", new DialogInterface.OnClickListener() {
+                    
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ShowMyDialog.this.finish();
+                    }
+                });
+                builder.setNegativeButton("No, no", new DialogInterface.OnClickListener() {
+                    
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Activity will continue", Toast.LENGTH_LONG).show();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+        }
+        return super.onCreateDialog(id);
     }
 }
